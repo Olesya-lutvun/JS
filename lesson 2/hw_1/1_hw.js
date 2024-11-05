10a402f73ac7fe0f124e89aa9bde45ed355e80bc
Olesya-lutvun <domchenko.olesya@gmail.com>
2024-11-05 14:38:48 +0200

    пункт 1 дз 2

---
+++ lesson 2/hw_1/1_hw.js
@@ -0,0 +1,16 @@
+let arr;
+arr = [153, -222, 76, -66, 77, 88, true, 22, 367, 666];
+console.log(arr)
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+
+
